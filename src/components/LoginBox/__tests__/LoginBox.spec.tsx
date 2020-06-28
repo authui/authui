242ba0466357046853fedc464acb59afb80e7006
Duc Ng <ducjava@gmail.com>
2020-06-27 23:56:17 -0700

    fix: visual tests (#2)
    * fix: visual tests
    
    * feat: add percy-storybook
    
    * fix: use env PERCY_TOKEN
    
    * fix: ci: PERCY_TOKEN

--- src/components/LoginBox/__tests__/LoginBox.spec.tsx
+++ src/components/LoginBox/__tests__/LoginBox.spec.tsx
@@ -9,8 +9,15 @@ import { act } from 'react-dom/test-utils';
 // This test reuses Component (with mocked data) in .stories.tsx file.
 // May need to mock APIs here also.
 
-describe('MyComp - jest test', () => {
-  it('works', async () => {
+describe('LoginBox', () => {
+  it('render', async () => {
+    const { container } = render(fullWidth());
+    expect(container.innerHTML).toMatch(/placeholder=\"Email\"/);
+    expect(container).toMatchSnapshot();
+    // (expect(await generateImage()) as any).toMatchImageSnapshot(); // TODO: find a way to inject CSS to puppeteer
+  });
+
+  it('click Submit - validation should fail', async () => {
     const { container, getByTestId } = render(fullWidth());
     expect(container.innerHTML).toMatch(/placeholder=\"Email\"/);
     act(() => {
@@ -19,7 +26,6 @@ describe('MyComp - jest test', () => {
     await waitFor(() => getByTestId('authui-error'));
 
     expect(container).toMatchSnapshot();
-    // (expect(await generateImage()) as any).toMatchImageSnapshot(); // TODO: find a way to inject CSS to puppeteer
-    await generateImage();
+    // (expect(await generateImage()) as any).toMatchImageSnapshot();
   });
 });
