41b8bb697b370c4e40e87e2aa592d16408af576b
ducwings <ducjava@gmail.com>
2020-06-29 23:56:09 -0700

    chore: test: update LoginBox.e2e.js

--- src/components/LoginBox/__tests__/LoginBox.e2e.js
+++ src/components/LoginBox/__tests__/LoginBox.e2e.js
@@ -1,3 +1,4 @@
+
 import { setupBrowser, newPage, takeScreenshot } from '../../../utils/e2eUtil';
 
 describe('LoginBox', () => {
@@ -8,36 +9,34 @@ describe('LoginBox', () => {
     let thisTest, element, page = await newPage(browser);
     expect().getTestData(testData => thisTest = testData);
 
-    await page.goto(`http://127.0.0.1:8080/iframe.html?id=components-loginbox--mobile-width`, {
+    await page.goto('http://127.0.0.1:8080/iframe.html?id=components-loginbox--mobile-width', {
       waitUntil: 'networkidle0'
     });
-
-    await (await page.$x(`//div[@id='authui-footer']/div/div/div`))[0].click();
-
-    await (await page.$x(`//*[@id="userId"]`))[0].click();
-
-    element = await page.$x(`//*[@id="userId"]`);
-    await element[0].type(`a@a.a`);
-
-    element = await page.$x(`//*[@id="password"]`);
-    await element[0].type(`a`);
-
-    await (await page.$x(`//div[@id='authui-footer']/div/div/div`))[0].click();
-
-    await (await page.$x(`//div[@id='authui-container']/div[2]/div/span`))[0].click();
-
-    await (await page.$x(`//div[@id='authui-form']/div[2]/div`))[0].click();
-
-    await (await page.$x(`//*[@id="userId"]`))[0].click();
-
-    element = await page.$x(`//*[@id="userId"]`);
-    await element[0].type(`demo1@gmail.com`);
-
-    element = await page.$x(`//*[@id="password"]`);
-    await element[0].type(`demo111`);
-
-    await (await page.$x(`//div[@id='authui-footer']/div/div/div`))[0].click();
-
+      
+    await (await page.$x(`//div[@id='authui-footer']/div[1]/div[1]/div[1]`))[0].click();
+      
+    await (await page.$x(`//input[@id='userId']`))[0].click();
+      
+    await (await page.$x(`//input[@id='userId']`))[0].type(`a@a.com`);
+      
+    await (await page.$x(`//input[@id='password']`))[0].type(`a`);
+      
+    await (await page.$x(`//div[@id='authui-footer']/div[1]/div[1]/div[1]`))[0].click();
+      
+    await (await page.$x(`//div[@id='authui-container']/div[2]/div[1]/span[1]`))[0].click();
+      
+    await (await page.$x(`//div[@id='authui-form']/div[2]/div[1]`))[0].click();
+      
+    await (await page.$x(`//input[@id='userId']`))[0].click();
+      
+    await (await page.$x(`//input[@id='userId']`))[0].type(`demo1@gmail.com`);
+      
+    await (await page.$x(`//input[@id='password']`))[0].click();
+      
+    await (await page.$x(`//input[@id='password']`))[0].type(`demo111`);
+      
+    await (await page.$x(`//div[@id='authui-footer']/div[1]/div[1]/div[1]`))[0].click();
+      
     await takeScreenshot(browser, page, `${thisTest.name}.png`);
   });
 
